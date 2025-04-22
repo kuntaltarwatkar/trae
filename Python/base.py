8336f3e83aa94b26f03353056104ea23497952b8
kuntaltarwatkar <tarwatkark@gmail.com>
2025-04-22 16:06:01 +0530

    Commit_1

--- Python/base.py
+++ Python/base.py
@@ -1,7 +1,25 @@
-def hello():
-    # return "Hello World!"
-    print("hello")
+import pandas as pandas
 
-hello()
+def get_data():
+    data = pandas.read_csv('data.csv')
+    return data
 
-print("hello")
+def get_question():
+    data = get_data()
+    question = data['question']
+    return question
+
+def get_answer():
+    data = get_data()
+    answer = data['answer']
+    return answer
+
+def get_answer_by_question(question):
+    data = get_data()
+    answer = data.loc[data['question'] == question, 'answer'].values[0]
+    return answer
+
+def get_question_by_answer(answer):
+    data = get_data()
+    question = data.loc[data['answer'] == answer, 'question'].values[0]
+    return question
