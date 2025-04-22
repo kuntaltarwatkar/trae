ade8208aecefc41b6cd24691d38ba4f1f4160278
kuntaltarwatkar <tarwatkark@gmail.com>
2025-04-22 18:22:07 +0530

    Everything committed

--- Javascript/basic.js
+++ Javascript/basic.js
@@ -1,8 +1,1 @@
 console.log("Hello World!");
-console.log("Hello World Again!");
-console.log("Hello World Again Again!");
-console.log("Hello World Again Again Again!");
-console.log("Hello World Again Again Again Again!");
-console.log("Hello World Again Again Again Again Again!");
-console.log("Hello World Again Again Again Again Again Again!");
-console.log("Hello World Again Again Again Again Again Again Again!");
